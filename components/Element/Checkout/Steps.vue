42596fc6cf84f5629a0f008ceb5c552f8749b315
Sean Metcalf <sean@seanmart.com>
2022-04-10 15:12:05 -0400

    updates

--- components/Element/Checkout/Steps.vue
+++ components/Element/Checkout/Steps.vue
@@ -2,7 +2,7 @@
   <div class="bg-black text-white font-semibold fill-lime flex flex-row justify-center items-center py-20 px-20 lg:px-40 pointer-events-none">
     <template v-for="(step,i) in steps">
       <div class="flex-auto flex flex-row items-center justify-center text-09 md:text-10" :class="{'opacity-40':active < i && !success}">
-        <span v-html="step.label" class="pr-10"/>
+        <span v-html="step.label" class="pr-05 md:pr-10"/>
         <div v-if="active <= i && !success" class="rounded-full h-10 w-10 border border-white"/>
         <Icon class="w-10 block" check v-else/>
       </div>
