e48e4b14eed3292b91b15b6c3f514947ebec5035
Sean Metcalf <sean@seanmart.com>
2022-06-16 13:17:57 -0400

    fixed sponsors

--- components/Title.vue
+++ components/Title.vue
@@ -1,15 +1,11 @@
 <template lang="html">
-  <div class="flex flex-row" :class="{'text-white': knockout}">
-    <div class="flex-initial">
-      <h3 class="whitespace-nowrap mb-10 font-header uppercase text-30 leading-09 font-bold">
-        <slot>{{value}}</slot>
-      </h3>
+  <div class="" :class="{'text-white': knockout}">
+      <h3 class="mb-10 font-header uppercase text-30 leading-09 font-bold"><slot>{{value}}</slot></h3>
       <div class="flex flex-row">
         <div class="bg-pink h-7px w-1/2" />
         <div class="bg-green h-7px w-1/2" />
+        <div class="flex-auto self-end h-7px w-full" :class="{'bg-white': knockout, 'bg-black': !knockout}"/>
       </div>
-    </div>
-    <div class="flex-auto self-end h-7px w-full" :class="{'bg-white': knockout, 'bg-black': !knockout}"/>
   </div>
 </template>
 
